5e7db117dbc74e99a2250eb75aaa2eb6264cd466
Priyanshii677 <priyanshisomani23105@gmail.com>
2023-01-31 23:58:40 +0530

    creating api endpoints (test)

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,25 @@
 import express from "express";
+import notes from "./data/notes.js";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
-app.listen(5000, console.log("server listening on port 5000"));
+app.get("/", (req, res) => {
+  res.send("api is running");
+});
+
+app.get("/api/notes", (req, res) => {
+  console.log(req, "req1");
+  res.json(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const note = notes.find((x) => x._id === req.params.id);
+
+  res.send(note);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(5000, console.log(`server listening on port ${PORT}`));

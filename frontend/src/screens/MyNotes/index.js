38cb1a53c6db49eb094ad713a50c9df6b53f8497
Priyanshii677 <priyanshisomani23105@gmail.com>
2023-02-04 20:53:43 +0530

    linter changes in mynotes ui

--- frontend/src/screens/MyNotes/index.js
+++ frontend/src/screens/MyNotes/index.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import s from "./MyNotes.module.scss";
 import Note from "../Note";
-import notes from "../../utils/notes.js";
+import axios from "axios";
 const MyNotes = () => {
+  const [notes, setNotes] = useState([]);
+
+  const fetchNotes = async () => {
+    const { data } = await axios.get("/api/notes");
+    setNotes(data);
+    console.log(data);
+  };
+  useEffect(() => {
+    console.log("hiiii");
+    fetchNotes();
+  }, []);
   return (
     <div className={s.mainContainer}>
       {notes.map((value) => {
-        return <Note title={value.title} content={value.content} />;
+        return (
+          <Note title={value.title} content={value.content} key={value._id} />
+        );
       })}
     </div>
   );

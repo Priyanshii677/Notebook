78fee1dc6a3a79027f2c829aed36fec18a7e6b9f
Priyanshii677 <priyanshisomani23105@gmail.com>
2023-02-14 23:13:48 +0530

    userAction file updated

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -13,10 +13,9 @@ import {
 import axios from "axios";
 
 export const login = (email, password) => async (dispatch) => {
-  console.log(email, password, "hhh");
-
   try {
     dispatch({ type: USER_LOGIN_REQUEST });
+
     const config = {
       headers: {
         "Content-type": "application/json",
@@ -25,21 +24,20 @@ export const login = (email, password) => async (dispatch) => {
 
     const { data } = await axios.post(
       "/api/users/login",
-      {
-        email,
-        password,
-      },
+      { email, password },
       config
     );
-    console.log(data, "ppp");
+
+    dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
+
     localStorage.setItem("userInfo", JSON.stringify(data));
-  } catch (err) {
+  } catch (error) {
     dispatch({
       type: USER_LOGIN_FAIL,
       payload:
-        err.response && err.response.data.message
-          ? err.response.data.message
-          : err.response,
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
     });
   }
 };
@@ -73,6 +71,39 @@ export const register = (name, email, password, pic) => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: USER_REGISTER_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const updateProfile = (user) => async (dispatch, getState) => {
+  try {
+    dispatch({ type: USER_UPDATE_REQUEST });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await axios.post("/api/users/profile", user, config);
+
+    dispatch({ type: USER_UPDATE_SUCCESS, payload: data });
+
+    dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
+
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: USER_UPDATE_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message

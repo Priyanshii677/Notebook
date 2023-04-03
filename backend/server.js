4f285a4df9a803e8873d503b354972eb4b5b0471
Priyanshii677 <priyanshisomani23105@gmail.com>
2023-04-03 23:29:58 +0530

    app deployment

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ import userRoutes from "./routes/userRoutes.js";
 import noteRoutes from "./routes/noteRoutes.js";
 import { errorHandler, notFound } from "./middlewares/errorMiddleware.js";
 import bodyParser from "body-parser";
+import path from "path";
+import src from "../frontend/";
 
 const app = express();
 dotenv.config();
@@ -29,6 +31,22 @@ app.use("/api/notes", noteRoutes);
 
 //   res.send(note);
 // });
+
+// --------------------------deployment------------------------------
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+// --------------------------deployment------------------------------
 
 app.use(notFound);
 app.use(errorHandler);

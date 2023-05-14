6058d6bc38841b47139ce52f497f4af8fdfdc77a
Priyanshii677 <priyanshisomani23105@gmail.com>
2023-05-14 23:51:39 +0530

    Improvements

--- frontend/src/screens/Note/index.js
+++ frontend/src/screens/Note/index.js
@@ -3,12 +3,11 @@ import { Link, useNavigate } from "react-router-dom";
 import s from "./Note.module.scss";
 import { motion } from "framer-motion";
 const Note = (props) => {
-  const [openModal, setOpenModal] = useState(false);
   const history = useNavigate();
   const { title, content, value } = props;
   const onClickHandlerNote = () => {
     // history(`/note/${value._id}`);
-    setOpenModal(true);
+    // setOpenModal(true);
   };
   return (
     <div className={s.mainContainer}>
@@ -39,55 +38,6 @@ const Note = (props) => {
       </div>
       {/* {openModal && <Modal />} */}
     </div>
-  );
-};
-
-const Modal = (props) => {
-  return (
-    <>
-      <motion.div
-        initial={{
-          opacity: 0,
-        }}
-        animate={{
-          opacity: 1,
-          transition: {
-            duration: 0.3,
-          },
-        }}
-        className={s.modalBackdrop}
-      />
-      <motion.div
-        initial={{
-          scale: 0,
-        }}
-        animate={{
-          scale: 1,
-          transition: {
-            duration: 0.3,
-          },
-        }}
-        style={{ backgroundColor: "blue" }}
-        className={s.modalContentWrapper}
-      >
-        <motion.div
-          initial={{
-            x: 100,
-          }}
-          animate={{
-            x: 0,
-            transition: {
-              delay: 0.3,
-              duration: 0.3,
-            },
-          }}
-          className={s.modalContent}
-          // dangerouslySetInnerHTML={createMarkup(content)}
-        >
-          hellllllo
-        </motion.div>
-      </motion.div>
-    </>
   );
 };
 
